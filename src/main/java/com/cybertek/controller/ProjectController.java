054135963e0d404cd409b0dadbedbc5f7abe146c
HuseyinCivan <huseyincivan@gmail.com>
2020-11-29 17:39:21 -0500

    jd_ticketing_project_mvc_project/create.html in progress

---
+++ src/main/java/com/cybertek/controller/ProjectController.java
@@ -0,0 +1,32 @@
+package com.cybertek.controller;
+
+import com.cybertek.dto.ProjectDTO;
+import com.cybertek.service.ProjectService;
+import com.cybertek.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/project")
+public class ProjectController {
+
+
+    @Autowired
+    ProjectService projectService;
+
+    @Autowired
+    UserService userService;
+
+    @GetMapping("/create")
+    public String createProject(Model model){
+
+        model.addAttribute("project", new ProjectDTO());
+        model.addAttribute("projects",projectService.findAll());
+        model.addAttribute("managers",userService.findAll());
+
+        return "/project/create";
+    }
+}
